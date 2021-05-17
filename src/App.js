1fa788234ff9270bb65c8ad81d413a8e396ea165
palak96 <palak.telang@gmail.com>
2021-05-17 13:17:07 +0530

    created till preview cards for api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
